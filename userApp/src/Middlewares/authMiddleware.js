c6a3c84c29b4bd0bec7f5ee2006de8188b1d4f0f
blair-developer <blairdeveloper17@gmail.com>
2026-05-21 09:53:32 +0300

    auth middleware

---
+++ userApp/src/Middlewares/authMiddleware.js
@@ -0,0 +1,11 @@
+function isAuthenticated(req, res, next) {
+
+    if (req.session && req.session.userId) {
+        next();
+    } else {
+        res.redirect("/");
+    }
+
+}
+
+module.exports = isAuthenticated;
