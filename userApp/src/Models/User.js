350cf0c3cbeb8184d0523c4b4252f6ab5aad45df
blair-developer <blairdeveloper17@gmail.com>
2026-05-22 09:03:39 +0300

    signup model updates

--- userApp/src/Models/User.js
+++ userApp/src/Models/User.js
@@ -2,9 +2,26 @@ const mongoose = require("mongoose");
 
 const Loginschema = new mongoose.Schema({
 
-    name: {
+    fullname: {
         type: String,
         required: true
+    },
+
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    profileImage: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
     },
 
     password: {
