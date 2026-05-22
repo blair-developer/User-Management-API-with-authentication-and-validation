a2f6bd22fd2908c66763bc2b848f378a994f58e2
blair-developer <blairdeveloper17@gmail.com>
2026-05-22 09:45:51 +0300

    Model tasks updated

--- userApp/src/Models/Task.js
+++ userApp/src/Models/Task.js
@@ -11,12 +11,26 @@ const taskSchema = new mongoose.Schema({
         type: String
     },
 
+     category: {
+        type: String,
+        default: "General"
+    },
+
+    status: {
+        type: String,
+        default: "Pending"
+    },
+
     userId: {
         type: String,
         required: true
     }
 
-});
+},
+  {
+    timestamps: true
+}
+);
 
 const Task = mongoose.model("Task", taskSchema);
 
